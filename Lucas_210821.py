5231afba380401b405fcd1afdb61a5f7587a6369
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-08-21 11:54:10 -0300

    Add files via upload

---
+++ Lucas_210821.py
@@ -0,0 +1,39 @@
+print("Question 1")
+print("   project: Lucas_210821")
+print("Question 2")
+j=[]
+def uw(o,m,g):
+    j.append(o)
+    j.append(m)
+    j.append(g)
+    return j
+print("Question 3")
+lj=uw("game","video","draw")
+print(lj)
+print("Question 4")
+ad={}
+def ltd(dl):
+    n=0
+    for asf in dl:
+        n+=1
+        an="a"+str(n)
+        ad[an]=asf
+    return ad
+print("Question 5")
+al=["random","dog","bye?"]
+ok=ltd(al)
+print(ok)
+print("Question 6")
+def wu(dc):
+    dtl=[]
+    for qwe in dc.values():
+        dtl.append(qwe)
+    return dtl
+print("Question 7")
+dtbl={"My name":"Lucas","Your name":"?"}
+sw=wu(dtbl)
+print(sw)
+print("Question 8")
+print("Send to GitHub :)")
+while True:
+	pass
